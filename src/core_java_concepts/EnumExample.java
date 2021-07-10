ef7b900308239e1e140d157f2f6d4bb74db3cf6f
rakeshkarkare <rakeshkarkare@gmail.com>
2021-07-10 17:23:47 +0400

    Commiting Enum Example

---
+++ src/core_java_concepts/EnumExample.java
@@ -0,0 +1,15 @@
+package core_java_concepts;
+
+enum Progamming_Lang{
+    JAVA, PYTHON, JAVASCRIPT;
+}
+
+public class EnumExample {
+
+    public static void main(String[] args){
+        Progamming_Lang pl = Progamming_Lang.JAVA;
+        System.out.println("Progamming Language is: "+ pl);
+    }
+
+
+}
