a2895f3cd5a42e2852d6b290aff25aa6e2acbcc9
rakeshkarkare <rakeshkarkare@gmail.com>
2021-08-16 23:12:35 +0400

    commiting main hashmap class

--- src/com/datastructures/hashtablesexamples/Main.java
+++ src/com/datastructures/hashtablesexamples/Main.java
@@ -11,6 +11,8 @@ public class Main {
         hmap.put(2, "Josh");
         hmap.put(3, "Karkare");
         hmap.put(3, "Rakesh");
+        hmap.put(4, null);
+        hmap.put(null, null);
         System.out.println(hmap);
     }
 
