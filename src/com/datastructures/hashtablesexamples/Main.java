a17a4182693e5178a33fc87f1ae77bbdadc49005
rakeshkarkare <rakeshkarkare@gmail.com>
2021-08-16 23:11:00 +0400

    commiting main hashmap class

---
+++ src/com/datastructures/hashtablesexamples/Main.java
@@ -0,0 +1,18 @@
+package com.datastructures.hashtablesexamples;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<Integer, String> hmap = new HashMap<>();
+        hmap.put(1, "Mosh");
+        hmap.put(2, "Josh");
+        hmap.put(3, "Karkare");
+
+        System.out.println(hmap);
+    }
+
+
+}
