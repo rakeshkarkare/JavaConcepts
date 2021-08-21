56e1683007f0488abcb169d0f03cca969ae89950
rakeshkarkare <rakeshkarkare@gmail.com>
2021-08-21 09:56:39 +0400

    commiting changes for primitive and reference types

---
+++ src/com/javaconcepts/TypesInJava/ReferenceTypeExample.java
@@ -0,0 +1,13 @@
+package com.javaconcepts.TypesInJava;
+
+import java.awt.*;
+
+public class ReferenceTypeExample {
+
+    public static void main(String[] args) {
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
+    }
+}
---
+++ src/com/javaconcepts/TypesInJava/TypesExample.java
@@ -0,0 +1,15 @@
+package com.javaconcepts.TypesInJava;
+
+import java.util.Date;
+
+public class TypesExample {
+
+    public static void main(String[] args) {
+
+        byte age= 30;
+        Date now = new Date();
+        System.out.println(now);
+    }
+
+
+}
