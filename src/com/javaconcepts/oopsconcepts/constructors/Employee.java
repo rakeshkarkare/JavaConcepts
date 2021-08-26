9b20d4b00e9678e68930afef6d763ced1a943570
rakeshkarkare <rakeshkarkare@gmail.com>
2021-08-26 23:25:58 +0400

    commiting changes for Constructor Overloading

---
+++ src/com/javaconcepts/oopsconcepts/constructors/Employee.java
@@ -0,0 +1,44 @@
+package com.javaconcepts.oopsconcepts.constructors;
+
+public class Employee {
+
+    private int hourlyRate;
+    private int baseSalary;
+
+    public Employee(int baseSalary){
+       this(baseSalary, 0);
+    }
+
+    public Employee(int baseSalary, int hourlyRate){
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
+
+    public int calculateWage(int extraHours){
+        return baseSalary + (getHourlyRate() * extraHours);
+    }
+
+    public int calculateWage(){
+        return calculateWage(0);
+    }
+
+    public void setBaseSalary(int baseSalary){
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or negative.");
+        this.baseSalary = baseSalary;
+    }
+
+    private int getBaseSalary(){
+        return baseSalary;
+    }
+
+    private int getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate < 0)
+            throw new IllegalArgumentException("Hourly Rate cannot be 0 or negative.");
+        this.hourlyRate = hourlyRate;
+    }
+}
