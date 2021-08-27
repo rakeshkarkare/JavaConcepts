2dbac008cf08b3b05f4f7976014ccfeb32268318
rakeshkarkare <rakeshkarkare@gmail.com>
2021-08-27 10:45:08 +0400

    extracted currency logic in constructor to intiatize

--- src/com/javaconcepts/mortagecalculator/MortgageReport.java
+++ src/com/javaconcepts/mortagecalculator/MortgageReport.java
@@ -5,10 +5,12 @@ import java.text.NumberFormat;
 public class MortgageReport {
 
 
+    private final NumberFormat currency;
     private MortgageCalculator calculator;
 
     public MortgageReport(MortgageCalculator calculator) {
         this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance();
     }
 
     public void printPaymentSchedule() {
@@ -16,13 +18,13 @@ public class MortgageReport {
         System.out.println("PAYMENT SCHEDULE");
         System.out.println("--------");
         for (double balance: calculator.getRemainingBalances()) {
-            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+            System.out.println(currency.format(balance));
         }
     }
 
     public void printMortgage() {
         double mortgage = calculator.calculateMortgage();
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        String mortgageFormatted = currency.format(mortgage);
         System.out.println();
         System.out.println("MORTGAGE");
         System.out.println("--------");
