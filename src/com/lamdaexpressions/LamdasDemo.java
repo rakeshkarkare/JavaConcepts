436c411adc491f0e2982f026630a17b3c1f67bf7
rakeshkarkare <rakeshkarkare@gmail.com>
2021-10-01 17:54:14 +0400

    commiting changes for Lambda expresssions

--- src/com/lamdaexpressions/LamdasDemo.java
+++ src/com/lamdaexpressions/LamdasDemo.java
@@ -2,13 +2,18 @@ package com.lamdaexpressions;
 
 public class LamdasDemo {
 
+    public static void print(String message){
+    }
+
     public static String prefix = "_";
     public static void show(){
-        greet(message -> System.out.println(prefix + message));
-        Printer printer = message -> System.out.println(message);
+        greet(message -> print(message));
+        greet(LamdasDemo::print);
+        // Class/Object ::method
     }
 
     public static void greet(Printer printer){
-        printer.print("Hello Python");
+        printer.print("Hell" +
+                "o Python");
     }
 }
