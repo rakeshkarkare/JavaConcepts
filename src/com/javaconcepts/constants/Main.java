c64b95d7ce296a8a39a1194f962444db1124054e
rakeshkarkare <rakeshkarkare@gmail.com>
2021-10-05 16:15:56 +0400

    commiting changes for constants

--- src/com/javaconcepts/constants/Main.java
+++ src/com/javaconcepts/constants/Main.java
@@ -3,6 +3,8 @@ package com.javaconcepts.constants;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Added final keyword so that java compiler will start treating as a constant variables.
         final float  PI = 3.14F;
         System.out.println(PI);
     }
