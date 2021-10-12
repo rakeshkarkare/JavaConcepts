7f16c78dabd7e4372f5e23e8d5b8a3631ae54f36
rakeshkarkare <rakeshkarkare@gmail.com>
2021-10-12 18:30:53 +0400

    commiting changes for git commands txt file

--- src/com/javaconcepts/oopsconcepts/encapsulation/Main.java
+++ src/com/javaconcepts/oopsconcepts/encapsulation/Main.java
@@ -1,7 +1,7 @@
 package com.javaconcepts.oopsconcepts.encapsulation;
 
-public class Main {
 
+public class Main {
     public static void main(String[] args) {
 
         var employee = new Employee();
@@ -9,6 +9,13 @@ public class Main {
         employee.setHourlyRate(20);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
+
+        test(employee);
+    }
+
+    public static void test(Employee employee){
+        if (employee instanceof Employee)
+            System.out.println("Yes, it is an instance of class Employee");
     }
 
 }
